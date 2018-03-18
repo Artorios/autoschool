5f450b4b0aae5ee7ef6507df966119e5f74adf59
d.melnyk <d.melnyk@tenantcloud.com>
2018-03-18 15:37:54 +0200

    Added on registration driver license category

--- database/migrations/2018_03_18_132011_add_category_for_student.php
+++ database/migrations/2018_03_18_132011_add_category_for_student.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDriverLicenseCategoryField extends Migration
+class AddCategoryForStudent extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,9 @@ class AddDriverLicenseCategoryField extends Migration
      */
     public function up()
     {
-        Schema::table()
+        Schema::table('users', function (Blueprint $table) {
+			$table->char('license_category', 1)->nullable()->after('phone');
+		});
     }
 
     /**
