e9fd6f9f873a0e9520f8f300f33b7bbb0ccb88ef
Djkey <djkey245@gmail.com>
2018-04-13 19:01:11 +0300

    AutoSchool cab

---
+++ routes/autoschool/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Models\Location\City;
+use App\Models\Location\PriceCity;
+use App\Models\User\UserLesson;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+Route::group(['prefix' => 'autoschool', 'namespace' => 'Autoschool', 'middleware' => 'auth'], function (){
+
+    Route::get('/', function () {
+        return view('autoschool.index.index');
+        })->name('autoschool.index');
+    });
+
+?>
