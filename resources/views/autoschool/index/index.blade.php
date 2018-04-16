ff778a68f151a40947c38e59aeec1d2209fc73a8
unknown <djkey245@gmail.com>
2018-04-16 22:57:25 +0300

    Valid

--- resources/views/autoschool/index/index.blade.php
+++ resources/views/autoschool/index/index.blade.php
@@ -9,18 +9,19 @@
         </div>
         <block-statistic></block-statistic>
         <div class="blockgroupe">
-            <h2>Группы:</h2>
+            <h2>Филиалы:</h2>
 
             <div class="table-wrapper">
                 <div class="title-line">
                     <span class="number">№</span>
-                    <span class="name">Название группы</span>
-                    <span class="data-and-time">Дата и время экзамена</span>
+                    <span class="count">Филиал</span>
+                    <span class="data-and-time">Адрес филиала</span>
                     <span class="count">Кол-во учеников</span>
+                    <span class="">Купоны активные/неактивные/всего</span>
                 </div>
                 <div class="line">
                     <div class="number">01</div>
-                    <div class="name"><a href="#">ИД-21</a></div>
+                    <div class="count"><a href="#">ИД-21</a></div>
                     <div class="data-and-time">
                         <img src="img/clock.png"> 02.09.2017 18:00
                     </div>
@@ -28,19 +29,10 @@
                         <span class="visible-xs hidden-sm">Количество учеников  8</span>
                         <span class="visible-sm hidden-xs visible-lg visible-md">8</span>
                     </div>
-                </div>
-                <div class="line">
-                    <div class="number">01</div>
-                    <div class="name"><a href="#">Группа супер</a></div>
-                    <div class="data-and-time">
-                        <img src="img/clock.png"> 02.09.2017 18:00
-                    </div>
-                    <div class="count">
-                      <span class="visible-xs hidden-sm">Количество учеников  8</span>
-                        <span class="visible-sm hidden-xs visible-lg">8</span>
+                    <span class="name">24 / 12 / 36</span>
 
-                    </div>
                 </div>
+
             </div>
         </div>
         <div class="invitegroupe">
@@ -54,7 +46,7 @@
             </ul>
             <div class="btn-wrapper">
                 <a href="#" class="btn-grey">
-                    Добавить группу
+                    Добавить филиал
                 </a>
             </div>
         </div>
@@ -62,10 +54,10 @@
             <form action="">
                 <div class="row nero">
                     <div class="col-xs-12 col-sm-4">
-                        <input id="name" type="text" class="name-group" placeholder="Название группы" required="">
+                        <input id="name" type="text" class="name-group" placeholder="Название филиала" required="">
                     </div>
                     <div class="col-xs-6 col-sm-2">
-                        <input id="date" type="text" class="data" placeholder="Дата" required="">
+                        <input id="date" type="text" class="data" placeholder="Адрес" required="">
                     </div>
                     <div class="col-xs-6 col-sm-2">
                         <input id="time" type="text" class="time" placeholder="Время" required="">
