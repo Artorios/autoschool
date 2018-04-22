a50d9c10364690802e0ac075853f01c50cbed119
Viktoriia Shakhova <viktori237t@gmail.com>
2018-04-22 21:46:07 +0300

    WIP: Investor finance/index.blade.php

--- routes/investor/web.php
+++ routes/investor/web.php
@@ -2,4 +2,5 @@
 
 Route::group(['prefix' => 'investor', 'namespace' => 'Investor', 'as' => 'investor.'], function(){
     Route::view('/', 'investor.index')->name('index');
+    Route::view('/finance', 'investor.finance.index')->name('finance.index');
 });
