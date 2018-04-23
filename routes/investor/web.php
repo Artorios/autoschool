03f4e4a59b7c4fe9db83866fd7ab27107ecd030c
Viktoriia Shakhova <viktori237t@gmail.com>
2018-04-23 21:46:27 +0300

    page investor

--- routes/investor/web.php
+++ routes/investor/web.php
@@ -7,4 +7,5 @@ Route::group(['prefix' => 'investor', 'namespace' => 'Investor', 'as' => 'invest
     Route::view('/profile/edit', 'investor.profile.edit')->name('profile.edit');
     Route::view('/coupons', 'investor.coupons.index')->name('coupons.index');
     Route::view('/coupons/add', 'investor.coupons.generate_coupon')->name('coupons.add');
+    Route::view('/school', 'investor.school.index')->name('school.index');
 });
