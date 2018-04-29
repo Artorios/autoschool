1b945d137b64e2cbfc9d1a3b9d8fa14dd792c8a5
guru <holova.dmitriy@gmail.com>
2018-04-30 01:02:38 +0300

    use cache

--- app/Http/Controllers/Investor/ProfileController.php
+++ app/Http/Controllers/Investor/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Investor;
 
 use Auth;
+use Cache;
 use App\Http\Controllers\Controller;
 use App\Models\User\InvestorProfile;
 use App\Http\Requests\Investor\UpdateProfileRequest;
@@ -11,23 +12,37 @@ class ProfileController extends Controller
 {
     public function show()
     {
+        $investor_id = Auth::id();
+
         return view('investor.profile.edit', [
-            'profile' => InvestorProfile::where('user_id', Auth::id())
-                ->with('legalEntity', 'individual', 'legalAddress', 'bankDetails', 'contact')
-                ->first(),
+            'profile' => Cache::rememberForever("investor_profile_$investor_id", function () use ($investor_id) {
+                return InvestorProfile::where('user_id', $investor_id)
+                    ->with('legalEntity', 'individual', 'legalAddress', 'bankDetails', 'contact')
+                    ->first();
+            }),
         ]);
     }
 
     public function update(UpdateProfileRequest $request)
     {
-        if (! $profile = InvestorProfile::where('user_id', Auth::id())->first()) {
-            return redirect()->back()->withInput($request->all())->withErrors(['Профиль инвестора не найден']);
+        $investor_id = Auth::id();
+
+        if (! $profile = InvestorProfile::where('user_id', $investor_id)->first()) {
+            return response()->json([
+                'errors' => ['Профиль инвестора не найден'],
+                'data' => false,
+            ], 404);
         }
 
         $profile->update([
             'active_config' => $request->get('active_config'),
         ]);
 
-        return redirect()->route('investor.profile.index')->with('messages', ['Профиль успешно обновлен']);
+        Cache::forget("investor_profile_$investor_id");
+
+        return response()->json([
+            'messages' => ['Профиль успешно обновлен'],
+            'data' => true
+        ], 204);
     }
 }
