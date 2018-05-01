426c3d7ff7e883c89b94bf2392245eb56a0aa907
Djkey <djkey245@gmail.com>
2018-05-01 17:14:39 +0300

    Start

---
+++ database/migrations/2018_05_01_134428_create_coupons_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('investor_id');
+            $table->integer('auto_school_id');
+            $table->integer('user_id')->unsigned();
+            $table->date('generation_date');
+            $table->date('activation_date')->nullable();
+            $table->integer('total');
+            $table->integer('commission');
+            $table->smallInteger('status')->comment('1-free; 2-sale;3-active;4-canceled');
+            $table->text('comment_investor')->nullable();
+            $table->text('comment_director')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
