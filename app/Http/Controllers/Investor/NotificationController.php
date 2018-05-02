4ec6e1dc29693b1c6b5f49009e159281bd4c6808
guru <holova.dmitriy@gmail.com>
2018-05-02 23:18:32 +0300

    investor notifications

---
+++ app/Http/Controllers/Investor/NotificationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Investor;
+
+use Illuminate\Http\Request;
+use App\Models\User\Notification;
+use App\Http\Controllers\Controller;
+
+class NotificationController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = Notification::where('user_id', $request->user()->id);
+
+        if ($request->get('show') == 'new') {
+            $data = $data->where('status', 1);
+        }
+
+        return view('investor.notifications.index', [
+            'notifications' => $data->paginate(10),
+        ]);
+    }
+
+    public function update(Notification $notification)
+    {
+        $this->authorize('read-notification', $notification);
+
+        $notification->update([
+            'status' => 0,
+        ]);
+
+        return response()->json([
+            'messages' => ['Уведомление прочитано'],
+            'data' => true,
+        ], 204);
+    }
+}
