dfe909e9aeeadf4b4b38aa2a18379a2d3e0bb939
Djkey <djkey245@gmail.com>
2018-05-16 13:03:24 +0300

    Fix

--- database/migrations/2018_05_16_095817_edit_image_in_users_table.php
+++ database/migrations/2018_05_16_095817_edit_image_in_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddImageInUser extends Migration
+class EditImageInUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,12 @@ class AddImageInUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('image');
+        });
+        Schema::table('users', function (Blueprint $table) {
             $table->string('image')->nullable();
         });
+
     }
 
     /**
