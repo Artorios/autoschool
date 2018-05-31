136af1268b85b98110ef5ce67ccba49ab4d6b013
Djkey <djkey245@gmail.com>
2018-05-31 11:37:30 +0300

    Fix Finance order string

--- app/Http/Controllers/Account/FinanceController.php
+++ app/Http/Controllers/Account/FinanceController.php
@@ -7,19 +7,30 @@ use App\Models\User\Contract;
 use App\Models\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 class FinanceController extends Controller
 {
     public function index()
     {
         $user = auth()->user();
+
+
+
         if(!$user->contract){
             $contract = Contract::create([
                 'name' => generateContractNumber($user),
                 'user_id' => $user->id
             ]);
         }
-        return view('account.finance.index', compact('user'));
+        $contract = [
+            'name' => $user->contract->name.$user->id.'_'.$user->contract->id.$user->city->id,
+            'date' => $user->created_at->format('d-m-Y'),
+            'price' => $user->city->price,
+            'amount' => $user->city->price
+
+        ];
+        return view('account.finance.index', compact('user','contract'));
     }
     public function getVariants(Request $request)
     {
