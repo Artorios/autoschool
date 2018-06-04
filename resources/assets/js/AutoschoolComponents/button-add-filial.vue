c9111db754971fe6f9d0e8350069c683c57a2e59
Mister Zhak <zhakgo@gmail.com>
2018-06-04 10:35:13 +0300

    add data icon

--- resources/assets/js/AutoschoolComponents/button-add-filial.vue
+++ resources/assets/js/AutoschoolComponents/button-add-filial.vue
@@ -13,9 +13,11 @@
                         <span class="error" v-if="errors.name">Введите имя филиала</span>
                         <input v-model.trim="data.name" v-bind:class="{'input-error': errors.name}" id="name" type="text" class="name-group" placeholder="Название филиала" required="">
                     </div>
-                    <div class="col-xs-6 col-sm-2">
+                    <div class="col-xs-6 col-sm-4">
                         <span class="error" v-if="errors.address">Введите адрес филиала</span>
-                        <input v-model.trim="data.address" v-bind:class="{'input-error': errors.address}" id="address" type="text" class="data" placeholder="Адрес филиала" required="">
+                        <div class="data">
+                            <input v-model.trim="data.address" v-bind:class="{'input-error': errors.address}" id="address" type="text" placeholder="Адрес филиала" required="">
+                        </div>
                     </div>
                     <div class="col-xs-12 col-sm-3">
                         <a href="#" class="btn-grey" v-on:click.prevent="sendDataToServer()">Сохранить</a>
